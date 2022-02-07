34a7b4d1568200b72e7eea8451fe4ce915fef4b5
dorrrn <doan7tran@gmail.com>
2022-02-07 17:01:14 +0100

    added delete page

--- routes/books.js
+++ routes/books.js
@@ -11,18 +11,18 @@ router.get("/", (req, res, next) => {
     .catch();
 });
 
-//===== Create route for /books/create
+//===== Create GET-route for /books/create
 // !! must be above :bookId, otherwise it will match it
 router.get("/create", (req, res, next) => {
   res.render("books/book-create");
 });
 
-// ===== Create route for submit page
+// ===== Create POST-route for books/create submit page
 router.post("/create", (req, res, next) => {
-  
-  const {title, author, description, rating} = req.body
-  const bookDetails = req.body
+  const { title, author, description, rating } = req.body;
+  const bookDetails = req.body;
 
+  // --> same as:
   // const bookDetails = {
   //   title: req.body.title,
   //   author: req.body.author,
@@ -31,17 +31,19 @@ router.post("/create", (req, res, next) => {
   // }
 
   Book.create(bookDetails)
-  .then((book) => {
-    res.redirect("/books")
-  })
-  .catch((err) => {
-    console.log('sorry', err)
-  })
-})
+    .then((book) => {
+      res.redirect("/books");
+    })
+    .catch((err) => {
+      console.log("sorry", err);
+    });
+});
 
 //==== Create route to book details
 router.get("/:bookId", (req, res, next) => {
-  Book.findById(req.params.bookId)
+  const bookId = req.params.bookId;
+
+  Book.findById(bookId)
     .then((bookDetails) => {
       //console.log(bookDetails)
       res.render("books/book-details", bookDetails);
@@ -49,6 +51,53 @@ router.get("/:bookId", (req, res, next) => {
     .catch();
 });
 
+//=== Create route for update book page
+router.get("/:bookId/edit", (req, res, next) => {
+  const bookId = req.params.bookId;
 
-//======
+  Book.findById(bookId)
+    .then((bookDetails) => {
+      res.render("books/book-edit", bookDetails);
+    })
+    .catch((err) => {
+      console.log("Error getting book details from DB...", err);
+    });
+});
+
+// ===== Create route for book edit page
+router.post("/:bookId/edit", (req, res, next) => {
+  const bookId = req.params.bookId;
+  const { title, author, description, rating } = req.body;
+  const newDetails = req.body;
+
+  // same as:
+  // const newDetails = {
+  //   title: req.body.title,
+  //   author: req.body.author,
+  //   description: req.body.description,
+  //   rating: req.body.rating
+  // };
+
+  Book.findByIdAndUpdate(bookId, newDetails)
+    .then(() => {
+      res.redirect(`/books/${bookId}`);
+    })
+    .catch((err) => {
+      console.log("Error updating book details", err);
+    });
+});
+
+//====== Create route for book delete page
+router.post("/:bookId/delete", (req, res, next) => {
+  const bookId = req.params.bookId;
+
+  Book.findByIdAndDelete(bookId)
+    .then(() => {
+      res.redirect("/books");
+    })
+    .catch((err) => {
+      console.log("Error deleting book", err);
+    });
+});
+
 module.exports = router;
