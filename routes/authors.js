deda631e58d3a0653c5f835f5c7369509214f442
dorrrn <doan7tran@gmail.com>
2022-02-08 16:54:07 +0100

    displayed number of books in authors details

--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,30 @@
 const router = require("express").Router();
 const Author = require("../models/Author.model");
+const Book = require("../models/Book.model");
 
 //==== Create route to /authors
 
 router.get("/", (req, res, next) => {
-  Author.find()
+  let books;
+
+  Book.find()
+    .then((booksFromDB) => {
+      books = booksFromDB;
+      return Author.find();
+    })
     .then((authorsFromDB) => {
-      res.render("authors/authors-list", { authors: authorsFromDB });
+      const authorsArr = authorsFromDB.map((authorDetails) => {
+        const { _id, name, age, country } = authorDetails;
+
+        const booksFromAuthor = books.filter((book) => {
+          return book.author.toString() == _id.toString();
+        });
+
+        const numberOfBooks = booksFromAuthor.length; // Length of array
+        return { name, age, country, numberOfBooks };
+      });
+
+      res.render("authors/authors-list", { authors: authorsArr });
     })
     .catch((err) => {
       console.log("Error getting authors from DB...", err);
